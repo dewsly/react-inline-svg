e2ba4c94acd31e844981a2f72d878f528a3e77d5
Chris Roush <croush513@hotmail.com>
2016-02-10 14:53:24 -0600

    Exposed SVGCache object to browserify bundles

--- test/commonjs/main.js
+++ test/commonjs/main.js
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var InlineSVG = require('react-inline-svg');
+var InlineSVG = require('react-inline-svg').InlineSVG;
+var SVGCache = require('react-inline-svg').SVGCache;
+
+SVGCache.instance.load("https://dbd6j53uzcole.cloudfront.net/assets/images/website/logo-grey.4979e9f5.svg", function(component) {
+  component.setState({loading: false});
+});
 
 var HelloWorld = React.createClass({
     render: function() {
